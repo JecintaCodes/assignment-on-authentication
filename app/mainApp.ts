33c496f58bec857badfd51920983b3970359cc8d
jecinta2003 <onyemaobijecintaugochi@gmail.com>
2023-08-02 02:19:44 +0100

    thir commit

---
+++ app/mainApp.ts
@@ -0,0 +1,8 @@
+import express, { Application } from "express"
+import cors from "cors"
+export const mainApp = (app:Application)=>{
+  app.use(express.json())
+  app.use(cors())
+
+  app.get("/")
+}
---
+++ index.ts
@@ -0,0 +1,29 @@
+import express,{ Application } from "express"
+import { mainApp } from "./app/mainApp"
+import { Authentication } from "./config/DataBase"
+
+const realport: number = parseInt(process.env.PORT!)
+const Port: number = realport
+const app: Application = express()
+mainApp(app)
+
+const server = app.listen(Port,()=>{
+    Authentication()
+    console.log(`server is listening on ${Port}..............`)
+}) 
+
+process.on("uncaughtException", (err: any)=>{
+    console.log(" serveris shutting down due to uncaughtException ")
+
+    console.log("uncaughtException", err)
+    process.exit(1)
+})
+process.on("unhandleRejection", (reason: any)=>{
+    console.log(" serveris shutting down due to unhandleRejection ")
+
+    console.log("unhandleRejection", reason)
+   
+    server.close = (()=>{
+        process.exit(1)
+    })
+})
