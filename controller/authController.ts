1a137570de8705cb56c30dc19f120e92e5eeafb1
jecinta2003 <onyemaobijecintaugochi@gmail.com>
2023-08-04 16:48:00 +0100

    second commit

--- controller/authController.ts
+++ controller/authController.ts
@@ -3,20 +3,20 @@ import authModel from "../model/authModel"
 import cloudinary from "../config/Cloudinary"
 
 
-export const createUser = async(req:Request, res:Response)=> {
+export const createUser = async(req:any, res:Response)=> {
     try {
 
         const {userName, email, password} = req.body;
 
 
-// const {secure_url, public_id} = await cloudinary.uploader.upload(req.file?.path)
+const {secure_url, public_id} = await cloudinary.uploader.upload(req.file?.path)
 
         const newUser = await authModel.create({
             userName,
             email,
             password, 
-            avatar : "img",
-            avatarID: "img",
+            avatar : secure_url,
+            avatarID: public_id,
         })
         
         console.log(newUser)
